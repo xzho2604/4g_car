d6ad0d792cf0c5606785ab4e49be0399ce1485b1
xzho2604 <erikxiangzhou@gmail.com>
2020-04-09 21:57:01 +1000

    change pystream to connect to nat bridge server

--- pystream/client.py
+++ pystream/client.py
@@ -6,29 +6,33 @@ import time
 import pickle
 import zlib
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('0.tcp.au.ngrok.io', 14313))
-connection = client_socket.makefile('wb')
+import numpy as np
+
+bridgeSever = '3.104.231.53'
+port = 8485
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#server_address = ("tcp://0.tcp.au.ngrok.io",15472)
+server_address = (bridgeSever,port)
 
 cam = cv2.VideoCapture(0)
-
 cam.set(3, 320);
 cam.set(4, 240);
 
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
 img_counter = 0
-
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
 while True:
     ret, frame = cam.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     result, frame = cv2.imencode('.jpg', frame, encode_param)
-#    data = zlib.compress(pickle.dumps(frame, 0))
+
     data = pickle.dumps(frame, 0)
     size = len(data)
 
-
     print("{}: {}".format(img_counter, size))
-    client_socket.sendall(struct.pack(">L", size) + data)
+    client_socket.sendto(struct.pack(">L", size) + data,server_address)
+
     img_counter += 1
 
 cam.release()
--- pystream/server.py
+++ pystream/server.py
@@ -6,26 +6,37 @@ import numpy as np
 import struct ## new
 import zlib
 
+
+def udpInit(host,port):
+    """create a upd sockt binding to the port listening to localhost"""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_address = (host,port)
+    sock.bind(server_address)
+    print("server bind to ", host, port)
+
+    return sock
+
+# =============================================================
 HOST=''
 PORT=8485
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s = udpInit(HOST,PORT)
 print('Socket created')
 
-s.bind((HOST,PORT))
-print('Socket bind complete')
-s.listen(10)
-print('Socket now listening')
 
-conn,addr=s.accept()
+# send init connect request to the bridge server
+s.sendto("server".encode(),('3.104.231.53',8485))
+print("sent init connection request to the bridge server")
+
 
 data = b""
 payload_size = struct.calcsize(">L")
 print("payload_size: {}".format(payload_size))
+
 while True:
     while len(data) < payload_size:
         print("Recv: {}".format(len(data)))
-        data += conn.recv(4096)
+        data += s.recvfrom(40960)[0]
 
     print("Done Recv: {}".format(len(data)))
     packed_msg_size = data[:payload_size]
@@ -33,11 +44,14 @@ while True:
     msg_size = struct.unpack(">L", packed_msg_size)[0]
     print("msg_size: {}".format(msg_size))
     while len(data) < msg_size:
-        data += conn.recv(4096)
+        data += s.recvfrom(40960)[0]
+        print("data len:",len(data))
     frame_data = data[:msg_size]
+    print("final frame len: ",len(frame_data))
     data = data[msg_size:]
+    frame= pickle.loads(frame_data)
+    #print(frame)
 
-    frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
     frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
     cv2.imshow('ImageWindow',frame)
     cv2.waitKey(1)
